ad3fb6e1eda84517775ba40d7c17c7ba674a468c
lsiten <1304906404@qq.com>
2018-06-21 01:25:34 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,7 @@
 
 <script>
 export default {
-  name: 'app',
-  created () {
-    let wx = this._wx
-    const permissions = JSON.stringify(['onMenuShareTimeline', 'onMenuShareAppMessage'])
-    const url = document.location.href
-    this.$store.dispatch('getWxConfig', {
-      url: encodeURIComponent(this.Trim(url.split('#')[0])),
-      jsApiList: permissions
-    }).then(data => {
-      wx.config(data)
-    })
-    wx.ready(() => {
-      console.log(111)
-    })
-  },
-  methods: {
-    Trim (str) {
-      return str.replace(/(^\/*)|(\/*$)/g, '')
-    }
-  }
+  name: 'app'
 }
 </script>
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,6 +5,26 @@
 </template>
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  created () {
+    let wx = this._wx
+    const permissions = JSON.stringify(['chooseImage'])
+    const url = document.location.href
+    this.$store.dispatch('getWxConfig', {
+      url: encodeURIComponent(this.Trim(url.split('#')[0])),
+      jsApiList: permissions
+    }).then(data => {
+      console.log(data)
+      wx.config(data)
+    })
+    wx.ready(() => {
+      console.log(111)
+    })
+  },
+  methods: {
+    Trim (str) {
+      return str.replace(/(^\/*)|(\/*$)/g, '')
+    }
+  }
 }
 </script>
